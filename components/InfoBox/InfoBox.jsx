74ebf7701f2f43a7b9b5e51e68c4600a05ac8982
John <64689339+john-ryabikov@users.noreply.github.com>
2025-09-25 15:18:39 +0300

    v1.0.1 Update InfoBox.jsx

--- components/InfoBox/InfoBox.jsx
+++ components/InfoBox/InfoBox.jsx
@@ -2,7 +2,7 @@ const InfoBox = ({children}) => {
   
   return (
     <div className='relative w-[320px] xl:w-auto flex flex-col items-center xl:items-start justify-center'>
-        <img className='absolute w-[450px] xl:w-auto top-0 left-[-290px] xl:left-[-415px]' src="img/Icons/dots.svg" alt="Dots"/>
+        <img className='absolute w-[450px] xl:w-auto top-0 left-[-290px] xl:left-[-415px]' src="/img/Icons/dots.svg" alt="Dots"/>
         {children}
     </div>
   )
