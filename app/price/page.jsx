8c6df3519a449f8c8e9f0b817e7ded773e7a5e65
John <64689339+john-ryabikov@users.noreply.github.com>
2025-09-25 15:25:22 +0300

    v1.0.1 Update price page.jsx

--- app/price/page.jsx
+++ app/price/page.jsx
@@ -6,7 +6,7 @@ import SectionAnimated from '@/components/SectionAnimated/SectionAnimated'
 const Section5 = () => {
   return (
     <SectionAnimated classSection='relative w-full pt-[140px] xl:pt-[160px] xl:pb-[100px] flex flex-col items-center justify-center'>
-      <img className='absolute w-[900px] xl:w-[1060px] top-[240px] left-0' src="img/Section-5/back_fon2.svg" alt=""/>
+      <img className='absolute w-[900px] xl:w-[1060px] top-[240px] left-0' src="/img/Section-5/back_fon2.svg" alt=""/>
       <div className='wrapper flex flex-col items-center xl:items-start justify-start pb-20 xl:pb-0'>
         <InfoBox>
           <h3 className='relative w-full font-title text-center xl:text-start text-[44px] xl:text-[66px] font-normal mb-2 pt-[46px] xl:pt-[60px]'>Стоимость услуг</h3>
