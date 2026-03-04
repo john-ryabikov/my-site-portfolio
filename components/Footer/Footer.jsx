8205abeeea7d120c94ff06456f5584a33c89d19c
John <64689339+john-ryabikov@users.noreply.github.com>
2026-03-04 14:55:33 +0300

    v1.2.1 (update Footer.jsx)

--- components/Footer/Footer.jsx
+++ components/Footer/Footer.jsx
@@ -7,7 +7,7 @@ const Footer = () => {
         <div className='wrapper flex flex-col-reverse xl:flex-row items-center xl:justify-between'>
             <p className='relative flex flex-col items-center xl:items-start justify-start mt-10 xl:mt-0 text-[11px]'>
                 <span className='relative font-extralight leading-[130%] tracking-[0.45px] text-[#ffffffb3]'>© Copyright 2015-{new Date().getFullYear()}. Все права защищены.</span>
-                <a className='relative font-light text-center xl:text-start leading-[130%] tracking-[0.45px] underline transition-all hover:opacity-40' href="blanks/Политика конфиденциальности персональных данных.pdf" target="_blank" rel="noreferrer">Политика конфедициальности и обработка пресональных данных</a>
+                <a className='relative font-light text-center xl:text-start leading-[130%] tracking-[0.45px] underline transition-all hover:opacity-40' href="/blanks/Политика конфиденциальности персональных данных.pdf" target="_blank" rel="noreferrer">Политика конфедициальности и обработка пресональных данных</a>
             </p>
             <p className='relative flex flex-row items-start justify-start'>
                 <span className='realtive text-[20px] font-thin text-start tracking-[0.78px] mr-[6px]'>Разработано на</span>
