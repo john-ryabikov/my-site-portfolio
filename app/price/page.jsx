c71afbe6975854d48b287ca934311902ddebfe52
John <64689339+john-ryabikov@users.noreply.github.com>
2026-03-04 14:44:14 +0300

    v1.2.0 (update price page.jsx)

--- app/price/page.jsx
+++ app/price/page.jsx
@@ -10,7 +10,7 @@ const Section5 = () => {
       <div className='wrapper flex flex-col items-center xl:items-start justify-start pb-20 xl:pb-0'>
         <InfoBox>
           <h3 className='relative w-full font-title text-center xl:text-start text-[44px] xl:text-[66px] font-normal mb-2 pt-[46px] xl:pt-[60px]'>Стоимость услуг</h3>
-          <p className='relative text-sm xl:text-lg text-[#ffffff59] mb-11 uppercase tracking-[0.4em]'>Прайс-лист</p>
+          <p className='relative text-sm xl:text-lg text-[#ffffff59] mb-11 uppercase tracking-[0.4em]'>Расценки</p>
         </InfoBox>
         <PricesTags/>
         <NewPrices/>
